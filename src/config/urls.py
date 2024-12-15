a9cf355e5ded873dac2b6426b81d34cb857ec340
joisemp <joise1124@gmail.com>
2024-12-15 23:31:46 +0530

    Created and configured app todo

--- src/config/urls.py
+++ src/config/urls.py
@@ -5,5 +5,6 @@ from core.views import LandingPageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing_page'),
-    path('core/', include('core.urls', namespace='core'))
+    path('core/', include('core.urls', namespace='core')),
+    path('todos/', include('todo.urls', namespace='todo')),
 ]
