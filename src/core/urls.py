405cf20e64aeca189988384c5f751dad0910c3db
joisemp <joise1124@gmail.com>
2024-12-15 23:27:24 +0530

    Created basic user authentication module

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
+from core.views import LoginView, UserRegisterView, LogoutView
 
 
-urlpatterns = []
+app_name = 'core'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(), name='logout')
+]
 
--- src/core/views.py
+++ src/core/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
+
+from django.contrib.auth.views import LoginView, LogoutView
+from core.forms import CustomAuthenticationForm, UserRegisterForm
+
+User = get_user_model()
 
 
 class LandingPageView(TemplateView):
     template_name = 'landing_page.html'
+
+
+class LoginView(LoginView):
+    form_class = CustomAuthenticationForm
+    template_name = 'core/login.html'
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return redirect('landing_page')
+    
+
+class UserRegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'core/register.html'
+    success_url = reverse_lazy('core:login')
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('landing_page')
+    
+
+class LogoutView(LogoutView):
+    template_name = 'core/logout.html'
+    
     
