892ddae8e0062b50d71116d0e809fb53e560940a
joisemp <joise1124@gmail.com>
2024-12-16 11:12:45 +0530

    Added style for login and register
    Co-authored-by: JosephSanthosh josephsanthosh121@gmail.com

--- src/core/forms.py
+++ src/core/forms.py
@@ -24,3 +24,13 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email',]  
+        
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.label_suffix = ''
+
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
