d3ce9fe853d7c7cf93cd19244e468eda083788d6
joisemp <joise1124@gmail.com>
2025-01-03 15:23:20 +0530

    Added password reset features

--- src/config/settings.py
+++ src/config/settings.py
@@ -116,3 +116,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- src/core/views.py
+++ src/core/views.py
@@ -4,7 +4,11 @@ from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, 
+    PasswordResetCompleteView, PasswordResetConfirmView, 
+    PasswordResetDoneView, PasswordResetView
+    )
 from core.forms import CustomAuthenticationForm, UserRegisterForm
 
 User = get_user_model()
@@ -43,3 +47,40 @@ class ChangePasswordView(PasswordChangeView):
     template_name = 'core/change_password.html'
     success_url = reverse_lazy('landing_page')
     
+    
+class ResetPasswordView(PasswordResetView):
+    email_template_name = 'core/password_reset/password_reset_email.html'
+    html_email_template_name = 'core/password_reset/password_reset_email.html'
+    subject_template_name = 'core/password_reset/password_reset_subject.txt'
+    success_url = reverse_lazy('core:done_password_reset')
+    template_name = 'core/password_reset/password_reset_form.html'
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+        })
+        return form
+
+
+class DonePasswordResetView(PasswordResetDoneView):
+    template_name = 'core/password_reset/password_reset_done.html'
+
+
+class ConfirmPasswordResetView(PasswordResetConfirmView):
+    success_url = reverse_lazy('core:complete_password_reset')
+    template_name = 'core/password_reset/password_reset_confirm.html'
+    
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class)
+        form.fields['new_password1'].widget.attrs.update({
+            'class': 'form-control',
+        })
+        form.fields['new_password2'].widget.attrs.update({
+            'class': 'form-control',
+        })
+        return form
+
+
+class CompletePasswordResetView(PasswordResetCompleteView):
+    template_name = 'core/password_reset/password_reset_complete.html'
