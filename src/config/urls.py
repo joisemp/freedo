35cfa0acccc0e71c2b9b45ccef1b9796b7dfd3a2
joisemp <joise1124@gmail.com>
2025-05-06 09:21:47 +0530

    Created apps and configured

--- src/config/urls.py
+++ src/config/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name='landing_page'),
     path('core/', include('core.urls', namespace='core')),
+    path('clients/', include('clients.urls', namespace='clients')),
+    path('payments/', include('payments.urls', namespace='payments')),
+    path('projects/', include('projects.urls', namespace='projects')),
+    path('tasks/', include('tasks.urls', namespace='tasks')),
 ]
---
+++ src/payments/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+app_name = 'payments'
+
+urlpatterns = [
+    # Define your URL patterns here
+    # Example:
+    # path('some-view/', SomeView.as_view(), name='some_view'),
+]
