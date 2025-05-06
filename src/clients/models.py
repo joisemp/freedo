db62fdebc6bd82dd456969f20f87b0b3e7b73426
joisemp <joise1124@gmail.com>
2025-05-06 09:28:37 +0530

    Created client model and generated migrations

--- src/clients/models.py
+++ src/clients/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+from config.utils import generate_unique_slug
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15, blank=True)
+    company = models.CharField(max_length=100, blank=True)
+    address = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    slug = models.SlugField(unique=True, max_length=100)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.name)
+            self.slug = generate_unique_slug(self, base_slug)
+        super().save(*args, **kwargs)
+
