fd89496aa860ac7f6d1ec751ce9e036e236cd4d4
joisemp <joise1124@gmail.com>
2025-05-06 22:12:46 +0530

    Created task list page

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
+from . views import TaskListView
 
 app_name = 'tasks'
 
 urlpatterns = [
-    # Define your URL patterns here
-    # Example:
-    # path('some-view/', SomeView.as_view(), name='some_view'),
+    path('projects/<slug:project_slug>/tasks/', TaskListView.as_view(), name='task_list'),
+    # path('projects/<slug:project_slug>/tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
+    # path('projects/<slug:project_slug>/tasks/create/', TaskCreateView.as_view(), name='task_create'),
+    # path('projects/<slug:project_slug>/tasks/<int:pk>/update/', TaskUpdateView.as_view(), name='task_update'),
+    # path('projects/<slug:project_slug>/tasks/<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    # path('projects/<slug:project_slug>/tasks/<int:pk>/complete/', TaskCompleteView.as_view(), name='task_complete'),
+    # path('projects/<slug:project_slug>/tasks/<int:pk>/uncomplete/', TaskUncompleteView.as_view(), name='task_uncomplete'
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from . models import Task
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+
+    def get_queryset(self):
+        project_slug = self.kwargs.get('project_slug')
+        return Task.objects.filter(project__slug=project_slug).order_by('-completed', 'due_date')
+
