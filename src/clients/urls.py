e1bb512e72ebe3a13f1ce25e98361db5197697a4
joisemp <joise1124@gmail.com>
2025-05-06 10:57:20 +0530

    Added view for client create

--- src/clients/urls.py
+++ src/clients/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ClientListView
+from .views import ClientListView, ClientCreateView
 
 app_name = 'clients'
 
 urlpatterns = [
     path('', ClientListView.as_view(), name='client_list'),
+    path('create/', ClientCreateView.as_view(), name='client_create'),
 ]
--- src/clients/views.py
+++ src/clients/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from . models import Client
+from . forms import ClientForm
 
 
 class ClientListView(ListView):
@@ -8,3 +10,15 @@ class ClientListView(ListView):
     template_name = 'clients/client_list.html'
     context_object_name = 'clients'
     
+class ClientCreateView(CreateView):
+    model = Client
+    template_name = 'clients/client_form.html'
+    form_class = ClientForm
+    success_url = reverse_lazy('clients:client_list')  
+    
+    def form_valid(self, form):
+        client = form.save(commit=False)
+        client.freelancer = self.request.user
+        client.save()
+        return super().form_valid(form)
+      
