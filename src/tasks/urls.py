85adad2337a76bf4693a91e4f188cc9675915e21
joisemp <joise1124@gmail.com>
2025-05-07 12:20:02 +0530

    added mark complete feature for tasks

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . views import TaskListView, TaskCreateView, TaskDeleteView, TaskUpdateView
+from . views import TaskListView, TaskCreateView, TaskDeleteView, TaskUpdateView, TaskCompleteView, TaskUncompleteView
 
 app_name = 'tasks'
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('<slug:project_slug>/create/', TaskCreateView.as_view(), name='task_create'),
     path('<slug:project_slug>/update/<slug:task_slug>/', TaskUpdateView.as_view(), name='task_update'),
     path('<slug:project_slug>/delete/<slug:task_slug>/', TaskDeleteView.as_view(), name='task_delete'),
+    path('<slug:project_slug>/complete/<slug:task_slug>/', TaskCompleteView.as_view(), name='task_complete'),
+    path('<slug:project_slug>/uncomplete/<slug:task_slug>/', TaskUncompleteView.as_view(), name='task_uncomplete'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -4,6 +4,8 @@ from . models import Task
 from projects.models import Project
 from . forms import TaskForm
 from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
+from django.views import View
 
 
 class TaskListView(ListView):
@@ -71,4 +73,26 @@ class TaskDeleteView(DeleteView):
     
     def get_success_url(self):
         return reverse('tasks:task_list', kwargs={'project_slug':self.kwargs['project_slug']})
-    
+
+
+class TaskCompleteView(View):
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(slug=kwargs['task_slug'])
+        task.completed = True
+        task.save()
+        return HttpResponseRedirect(reverse('tasks:task_list', kwargs={'project_slug': kwargs['project_slug']}))
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class TaskUncompleteView(View):
+    def post(self, request, *args, **kwargs):
+        task = Task.objects.get(slug=kwargs['task_slug'])
+        task.completed = False
+        task.save()
+        return HttpResponseRedirect(reverse('tasks:task_list', kwargs={'project_slug': kwargs['project_slug']}))
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
