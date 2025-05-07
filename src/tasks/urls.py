b22d2c23dc6ac9024856a68a9e06cb47249c9525
joisemp <joise1124@gmail.com>
2025-05-07 12:07:32 +0530

    added task update view

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . views import TaskListView, TaskCreateView, TaskDeleteView
+from . views import TaskListView, TaskCreateView, TaskDeleteView, TaskUpdateView
 
 app_name = 'tasks'
 
 urlpatterns = [
-    path('projects/<slug:project_slug>/tasks/', TaskListView.as_view(), name='task_list'),
-    path('projects/<slug:project_slug>/tasks/create/', TaskCreateView.as_view(), name='task_create'),
-    path('projects/<slug:project_slug>/tasks/<slug:task_slug>/delete/', TaskDeleteView.as_view(), name='task_delete'),
+    path('<slug:project_slug>/', TaskListView.as_view(), name='task_list'),
+    path('<slug:project_slug>/create/', TaskCreateView.as_view(), name='task_create'),
+    path('<slug:project_slug>/update/<slug:task_slug>/', TaskUpdateView.as_view(), name='task_update'),
+    path('<slug:project_slug>/delete/<slug:task_slug>/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -41,6 +41,22 @@ class TaskCreateView(CreateView):
         return reverse('tasks:task_list', kwargs={'project_slug':self.kwargs['project_slug']})
     
 
+class TaskUpdateView(UpdateView):
+    model = Task
+    template_name = 'tasks/task_update_form.html'
+    form_class = TaskForm
+    slug_field = 'slug'
+    slug_url_kwarg = 'task_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project_slug"] = self.kwargs['project_slug']
+        return context
+    
+    def get_success_url(self):
+        return reverse('tasks:task_list', kwargs={'project_slug':self.kwargs['project_slug']})
+    
+
 class TaskDeleteView(DeleteView):
     model = Task
     template_name = 'tasks/task_confirm_delete.html'
