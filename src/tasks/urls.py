4d90676b5334b1441f4311682fd693ce66c87c18
joisemp <joise1124@gmail.com>
2025-05-07 11:58:09 +0530

    Added task delete view

--- src/tasks/urls.py
+++ src/tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from . views import TaskListView, TaskCreateView
+from . views import TaskListView, TaskCreateView, TaskDeleteView
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('projects/<slug:project_slug>/tasks/', TaskListView.as_view(), name='task_list'),
-    path('projects/<slug:project_slug>/tasks/create/', TaskCreateView.as_view(), name='task_create')
+    path('projects/<slug:project_slug>/tasks/create/', TaskCreateView.as_view(), name='task_create'),
+    path('projects/<slug:project_slug>/tasks/<slug:task_slug>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
--- src/tasks/views.py
+++ src/tasks/views.py
@@ -40,3 +40,19 @@ class TaskCreateView(CreateView):
     def get_success_url(self):
         return reverse('tasks:task_list', kwargs={'project_slug':self.kwargs['project_slug']})
     
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    context_object_name = 'task'
+    slug_field = 'slug'
+    slug_url_kwarg = 'task_slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project_slug"] = self.kwargs['project_slug']
+        return context
+    
+    def get_success_url(self):
+        return reverse('tasks:task_list', kwargs={'project_slug':self.kwargs['project_slug']})
+    
