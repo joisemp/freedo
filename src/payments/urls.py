f8eaf6dd000a42d84a9f402fe83b81f0a8666e01
joisemp <joise1124@gmail.com>
2025-05-08 21:30:40 +0530

    Added payment list view

--- src/payments/urls.py
+++ src/payments/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
+from .views import PaymentListView
 
 app_name = 'payments'
 
 urlpatterns = [
-    # Define your URL patterns here
-    # Example:
-    # path('some-view/', SomeView.as_view(), name='some_view'),
+    path('<slug:project_slug>/', PaymentListView.as_view(), name='payment_list'),
 ]
--- src/payments/views.py
+++ src/payments/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from . models import Payment
 
-# Create your views here.
+
+class PaymentListView(ListView):
+    model = Payment
+    template_name = 'payments/payment_list.html'
+    context_object_name = 'payments'
+
+    def get_queryset(self):
+        project_slug = self.kwargs.get('project_slug')
+        return Payment.objects.filter(project__slug=project_slug).order_by('-date')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["project_slug"] = self.kwargs['project_slug']
+        return context
